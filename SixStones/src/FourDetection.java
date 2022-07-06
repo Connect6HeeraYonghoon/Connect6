4c3a97c7d5091f2b1fd4e0516e04111c37c99259
YonghoonChung <yonghoon1999@gmail.com>
2022-07-06 13:39:25 +0900

    edit

--- SixStones/src/FourDetection.java
+++ SixStones/src/FourDetection.java
@@ -1,47 +1,44 @@
 
 public class FourDetection {
 	int[][] endPoint = new int[8][2];
+
 	public void checkFourDetection(int x, int y) {
 		if (checkHorizontal(x, y) > 4) {
 			Calculation.weight[endPoint[2][0]][endPoint[2][1]] = -50;
 			Calculation.weight[endPoint[6][0]][endPoint[6][1]] = -50;
 			System.out.println("right :" + endPoint[2][0] + " " + endPoint[2][1]);
-			System.out.println("left :" +endPoint[6][0] + " " + endPoint[6][1]);
+			System.out.println("left :" + endPoint[6][0] + " " + endPoint[6][1]);
 			return;
 		}
 		if (checkVertical(x, y) > 4) {
 			Calculation.weight[endPoint[0][0]][endPoint[0][1]] = -50;
 			Calculation.weight[endPoint[4][0]][endPoint[4][1]] = -50;
 			System.out.println("up :" + endPoint[0][0] + " " + endPoint[0][1]);
-			System.out.println("down :" +endPoint[4][0] + " " + endPoint[4][1]);
-			return ;
+			System.out.println("down :" + endPoint[4][0] + " " + endPoint[4][1]);
+			return;
 		}
 		if (checkDiagonal(x, y) > 4) {
 			Calculation.weight[endPoint[7][0]][endPoint[7][1]] = -50;
 			Calculation.weight[endPoint[3][0]][endPoint[3][1]] = -50;
 			System.out.println(" UpperLeft :" + endPoint[7][0] + " " + endPoint[7][1]);
-			System.out.println(" LowerRight :" +endPoint[3][0] + " " + endPoint[3][1]);
+			System.out.println(" LowerRight :" + endPoint[3][0] + " " + endPoint[3][1]);
 			return;
 		}
-	}
-
-			
 		if (checkReverseDiagonal(x, y) > 4) {
 			Calculation.weight[endPoint[5][0]][endPoint[5][1]] = -50;
 			Calculation.weight[endPoint[1][0]][endPoint[1][1]] = -50;
 			System.out.println("LLD :" + endPoint[5][0] + " " + endPoint[5][1]);
-			System.out.println("URU :" +endPoint[1][0] + " " + endPoint[1][1]);
-			return ;
+			System.out.println("URU :" + endPoint[1][0] + " " + endPoint[1][1]);
+			return;
 		}
 	}
-	
-	
-	//vertical check
+
+	// vertical check
 	private int checkVertical(int x, int y) {
 		return checkUp(x, y, GamePanel.bwMatrix[x][y]) + checkDown(x, y, GamePanel.bwMatrix[x][y]);
 	}
-  
-  private int checkUp(int x, int y, int stoneColor) {
+
+	private int checkUp(int x, int y, int stoneColor) {
 		try {
 			if (GamePanel.bwMatrix[x][y] != stoneColor) {
 				endPoint[4][0] = x;
@@ -66,7 +63,8 @@ public class FourDetection {
 			return 0;
 		}
 	}
-	//Horizontal check
+
+	// Horizontal check
 	private int checkHorizontal(int x, int y) {
 		return checkLeft(x, y, GamePanel.bwMatrix[x][y]) + checkRight(x, y, GamePanel.bwMatrix[x][y]);
 	}
@@ -79,7 +77,12 @@ public class FourDetection {
 				return 0;
 			}
 			return checkRight(x + 1, y, stoneColor) + 1;
-	
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return 0;
+		}
+	}
+
 	private int checkLeft(int x, int y, int stoneColor) {
 		try {
 			if (GamePanel.bwMatrix[x][y] != stoneColor) {
@@ -92,8 +95,8 @@ public class FourDetection {
 			return 0;
 		}
 	}
-	
-  private int checkDiagonal(int x, int y) {
+
+	private int checkDiagonal(int x, int y) {
 		return checkULU(x, y, GamePanel.bwMatrix[x][y]) + checkLRD(x, y, GamePanel.bwMatrix[x][y]);
 	}
 
@@ -101,29 +104,30 @@ public class FourDetection {
 		try {
 			if (GamePanel.bwMatrix[x][y] != stoneColor) {
 				endPoint[3][0] = x;
-				endPoint[3][1] = y;	
+				endPoint[3][1] = y;
 				return 0;
 			}
 			return checkLRD(x + 1, y + 1, stoneColor) + 1;
-    } catch (ArrayIndexOutOfBoundsException e) {
+		} catch (ArrayIndexOutOfBoundsException e) {
 			return 0;
 		}
 	}
-  private int checkULU(int x, int y, int stoneColor) {
+
+	private int checkULU(int x, int y, int stoneColor) {
 		try {
 			if (GamePanel.bwMatrix[x][y] != stoneColor) {
-						
+
 				endPoint[7][0] = x;
 				endPoint[7][1] = y;
 				return 0;
 			}
 			return checkULU(x - 1, y - 1, stoneColor) + 1;
-      } catch (ArrayIndexOutOfBoundsException e) {
+		} catch (ArrayIndexOutOfBoundsException e) {
 			return 0;
 		}
 	}
-  
-	//ReverseDiagonal check
+
+	// ReverseDiagonal check
 	private int checkReverseDiagonal(int x, int y) {
 		return checkURU(x, y, GamePanel.bwMatrix[x][y]) + checkLLD(x, y, GamePanel.bwMatrix[x][y]);
 	}
@@ -149,6 +153,8 @@ public class FourDetection {
 				return 0;
 			}
 			return checkURU(x + 1, y - 1, stoneColor) + 1;
-
-		
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return 0;
+		}
+	}
 }
