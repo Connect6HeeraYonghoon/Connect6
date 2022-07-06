1e1e96ef666dfb97a83e5865ded283fa074f2346
YonghoonChung <yonghoon1999@gmail.com>
2022-07-06 11:54:24 +0900

    곧 없어질것들

--- SixStones/src/Calculation.java
+++ SixStones/src/Calculation.java
@@ -3,7 +3,7 @@ import java.awt.Color;
 
 public class Calculation {
 	int[][] weight = new int[19][19];
-	int[][] weightStatus = new int[19][19]; //없으면 0이고,검은색 1, 흰색 2, 착수 3
+	int[][] weightStatus = new int[19][19]; 	//없으면 0이고,검은색 1, 흰색 2, 착수 3
 
 	final static int blackStone = 200;
 	final static int whiteStone = 300;
@@ -23,7 +23,7 @@ public class Calculation {
 		int y = Memory.points.get(Memory.points.size() - 1).j;
 		int min = 10000000;
 		try {
-			if (Memory.points.get(Memory.points.size() - 1).color == Color.BLACK) { // 검은색
+			if (Memory.points.get(Memory.points.size() - 1).color == Color.BLACK) { // 寃����깋
 //				if(weightStatus[x][y] == 0)
 				weight[x][y - 1] += -1;
 				weight[x + 1][y - 1] += -1;
@@ -37,7 +37,7 @@ public class Calculation {
 				weightStatus[x][y] = 1;
 				weight[x][y] = blackStone;
 				
-			} else if (Memory.points.get(Memory.points.size() - 1).color == Color.WHITE) {// 흰색
+			} else if (Memory.points.get(Memory.points.size() - 1).color == Color.WHITE) {// �씛�깋
 				weight[x][y - 1] += 1;
 				weight[x + 1][y - 1] += 1;
 				weight[x + 1][y] += 1;
